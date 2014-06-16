6cdac4d8ccddf4c6a997c568e21f3b92677083cd
Brent Ertz <brent.ertz@gmail.com>
2014-06-16 08:43:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# BackboneRouteControl
+
+
+BackboneRouteControl adds support for __controller#action__ style route action definitions, enabling one to define route callbacks on separate controller/handler objects.
+
+## Installation
+
+Using [Bower](http://twitter.github.com/bower/) install `bower install backbone-route-control` or simply copy [backbone-route-control.js](https://raw.github.com/brentertz/backbone-route-control/master/backbone-route-control.js)
+
+## Usage
+
+Coming soon...
+
+## Tests
+
+Coming soon...
---
+++ backbone-route-control.js
@@ -0,0 +1,42 @@
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['underscore', 'backbone'], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('underscore'), require('backbone'));
+  } else {
+    factory(root._, root.Backbone);
+  }
+})(this, function(_, Backbone) {
+  var BackboneRouteControl = Backbone.Router.extend({
+    constructor: function(options) {
+      options || (options = {});
+      if (options.controllers) { this.controllers = options.controllers; }
+      Backbone.Router.prototype.constructor.call(this, options);
+    },
+
+    _bindRoutes: function() {
+      if (!this.routes) { return; }
+      this.routes = _.result(this, 'routes');
+      var route;
+      var routes = _.keys(this.routes);
+      while ((route = routes.pop()) != null) {
+        var routeAction = this.routes[route];
+        var routeParts = routeAction.split('#');
+        var isControllerAction = routeParts.length === 2;
+
+        if (isControllerAction) {
+          var controller, controllerName, method, methodName;
+          controllerName = routeParts[0];
+          controller = this.controllers[controllerName];
+          methodName = routeParts[1];
+          method = controller[methodName];
+          this.route(route, routeAction, _.bind(method, controller));
+        } else {
+          this.route(route, routeAction);
+        }
+      }
+    }
+  });
+
+  return BackboneRouteControl;
+});
